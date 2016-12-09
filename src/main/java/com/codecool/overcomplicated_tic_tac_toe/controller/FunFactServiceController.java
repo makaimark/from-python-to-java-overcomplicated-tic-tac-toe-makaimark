41d0a35edb7f011c2e8e3ab4feee433f4952aac8
makaimark <makaimark@gmail.com>
2016-12-09 11:12:28 +0100

    a little correction

--- src/main/java/com/codecool/overcomplicated_tic_tac_toe/controller/FunFactServiceController.java
+++ src/main/java/com/codecool/overcomplicated_tic_tac_toe/controller/FunFactServiceController.java
@@ -15,7 +15,7 @@ import java.util.Arrays;
 public class FunFactServiceController {
     private static final Logger logger = LoggerFactory.getLogger(FunFactServiceController.class);
     private static final String SERVICE_URL = "http://localhost:60000";
-    private  static  final String SELECTED_CATEGORY = "food";
+    private  static  final String FunFactCategoryValue = "food";
 
     public Boolean isRunning() throws URISyntaxException, IOException {
         logger.info("Checking Service status");
@@ -52,10 +52,10 @@ public class FunFactServiceController {
     public static String getRandomByCategory() throws URISyntaxException, IOException {
         logger.info("Send request to the Service, we would like to Get a random joke by category");
         ArrayList categories = getCategories();
-        if ( !categories.contains(SELECTED_CATEGORY)) {
+        if ( !categories.contains(FunFactCategoryValue)) {
             throw new IllegalArgumentException();
         }
-        String result = execute("/api/getbycategory?category="+SELECTED_CATEGORY);
+        String result = execute("/api/getbycategory?category="+FunFactCategoryValue);
         return result;
     }
 
