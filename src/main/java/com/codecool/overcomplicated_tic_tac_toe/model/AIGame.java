542092369d2015e6625912ee99d26bbdbd55578d
makaimark <makaimark@gmail.com>
2017-01-03 15:25:55 +0100

    interface and enum ready for the game

---
+++ src/main/java/com/codecool/overcomplicated_tic_tac_toe/model/AIGame.java
@@ -0,0 +1,16 @@
+package com.codecool.overcomplicated_tic_tac_toe.model;
+
+import java.util.List;
+
+/**
+ * Created by makaimark on 2017.01.03..
+ */
+public interface AIGame {
+
+    void setInitialState();
+    List getState();
+    void setUserStep(int place);
+    void setComputerStep(int place);
+    GameResult getGameResult();
+
+}
---
+++ src/main/java/com/codecool/overcomplicated_tic_tac_toe/model/GameResult.java
@@ -0,0 +1,5 @@
+package com.codecool.overcomplicated_tic_tac_toe.model;
+
+public enum GameResult {
+    NO_WINNER, USER_WON, COMPUTER_WON, TIE_GAME
+}
